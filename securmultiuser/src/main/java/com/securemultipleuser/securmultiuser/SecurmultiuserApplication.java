4c077729181adcbc7d84baf7a6b4e2c8511eb021
Mohan kumar <pmohankumar550@gmail.com>
2025-07-23 21:20:40 +0530

    secure multiuser

---
+++ securmultiuser/src/main/java/com/securemultipleuser/securmultiuser/SecurmultiuserApplication.java
@@ -0,0 +1,13 @@
+package com.securemultipleuser.securmultiuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurmultiuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurmultiuserApplication.class, args);
+	}
+
+}
---
+++ securmultiuser/src/test/java/com/securemultipleuser/securmultiuser/SecurmultiuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securemultipleuser.securmultiuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurmultiuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
